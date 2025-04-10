e14f431a6faea532f85cded26a9ac5d243d31e08
Stefan Dieterle <stefand@artcom.de>
2025-04-10 10:05:06 +0200

    Add authentication

--- custom_images/views.py
+++ custom_images/views.py
@@ -1,9 +1,14 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 from .models import CustomImage
 from .serializers import CustomImageModelSerializer
 
 
 class CustomImageViewSet(ReadOnlyModelViewSet):
+    """
+    A viewset for listing images. All authenticated users are allowed to view images
+    """
+    permission_classes = [IsAuthenticated]
     serializer_class = CustomImageModelSerializer
     queryset = CustomImage.objects.all()
