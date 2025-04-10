ef5352c4b952f0a07dfd7bca7ab014cd17cb6882
Stefan Dieterle <stefand@artcom.de>
2025-04-10 16:35:14 +0200

    Add live field on image

--- custom_images/serializers.py
+++ custom_images/serializers.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.db import models  # noqa
 from rest_framework import serializers
-
+from wagtail.models import Page
 from .models import CustomImage
 
 
@@ -12,8 +12,19 @@ class RenditionFileField(serializers.RelatedField):
 
 class CustomImageModelSerializer(serializers.ModelSerializer):
     renditions = RenditionFileField(many=True, read_only=True)
+    live = serializers.SerializerMethodField()
 
     class Meta:
         model = CustomImage
-        fields = ["id", "title", "file", "public", "renditions"]
+        fields = ["id", "title", "file", "public", "renditions", "live"]
         depth = 1
+
+    def get_live(self, obj: CustomImage) -> bool:
+        if len(obj.get_usage()) > 0:
+            referenced_live_pages = [reference_index[0]
+                                     for reference_index in obj.get_usage()
+                                     if isinstance(reference_index[0], Page)
+                                     and reference_index[0].live]
+            return len(referenced_live_pages) > 0
+        else:
+            return False
