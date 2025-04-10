b48b61e862cba16b540f65a110962400ac22361a
Stefan Dieterle <stefand@artcom.de>
2025-04-10 11:26:30 +0200

    Show renditions

--- custom_images/serializers.py
+++ custom_images/serializers.py
@@ -5,12 +5,18 @@ from rest_framework import serializers
 from .models import CustomImage
 
 
+class RenditionFileField(serializers.RelatedField):
+    def to_representation(self, value):
+        return settings.WAGTAILADMIN_BASE_URL + str(value.file.url)
+
+
 class CustomImageModelSerializer(serializers.ModelSerializer):
     imageUrl = serializers.SerializerMethodField()
+    renditions = RenditionFileField(many=True, read_only=True)
 
     class Meta:
         model = CustomImage
-        fields = ['id', 'title', 'imageUrl', 'file', 'public']
+        fields = ["id", "title", "imageUrl", "public", "renditions"]
         depth = 1
 
     def get_imageUrl(self, obj: CustomImage) -> str:
