e31c792b1ce9cc66a6593002cf052b4f25d173e9
Stefan Dieterle <stefand@artcom.de>
2025-05-07 11:44:44 +0200

    Add self URL and children

--- experience/serializers.py
+++ experience/serializers.py
@@ -2,16 +2,26 @@ from django.conf import settings
 from django.db import models  # noqa
 from rest_framework import serializers
 
-from .models import Welcome, CharacterOverview, ChooseCharacter
+from .models import CharacterOverview, ChooseCharacter, Welcome
 
 
 class WelcomeModelSerializer(serializers.ModelSerializer):
     backgroundImageUrl = serializers.SerializerMethodField()
     siteName = serializers.SerializerMethodField()
+    children = serializers.SerializerMethodField()
+    selfUrl = serializers.SerializerMethodField()
 
     class Meta:
         model = Welcome
-        fields = ["id", "title", "description", "siteName", "backgroundImageUrl"]
+        fields = [
+            "id",
+            "title",
+            "description",
+            "siteName",
+            "backgroundImageUrl",
+            "children",
+            "selfUrl",
+        ]
         depth = 1
 
     def get_backgroundImageUrl(self, obj: Welcome) -> str:
@@ -20,11 +30,26 @@ class WelcomeModelSerializer(serializers.ModelSerializer):
     def get_siteName(self, obj: Welcome) -> str:
         return obj.site_name if obj.site_name else ""
 
+    def get_children(self, obj: Welcome) -> list:
+        children = obj.get_children().live().specific()
+        serializer = CharacterOverviewModelSerializer(children, many=True)
+        return serializer.data if serializer.data else []
+
+    def get_selfUrl(self, obj: Welcome) -> str:
+        return (
+            settings.WAGTAILADMIN_BASE_URL
+            + settings.API_BASE_URL
+            + "/welcome/"
+            + str(obj.id)
+        )
+
 
 class CharacterOverviewModelSerializer(serializers.ModelSerializer):
     charactersImageUrl = serializers.SerializerMethodField()
     backgroundImageUrl = serializers.SerializerMethodField()
     siteName = serializers.SerializerMethodField()
+    children = serializers.SerializerMethodField()
+    selfUrl = serializers.SerializerMethodField()
 
     class Meta:
         model = CharacterOverview
@@ -35,6 +60,8 @@ class CharacterOverviewModelSerializer(serializers.ModelSerializer):
             "backgroundImageUrl",
             "charactersImageUrl",
             "onboarding",
+            "children",
+            "selfUrl",
         ]
         depth = 1
 
@@ -45,13 +72,30 @@ class CharacterOverviewModelSerializer(serializers.ModelSerializer):
         return Welcome.objects.get(id=obj.get_parent().id).site_name
 
     def get_backgroundImageUrl(self, obj: CharacterOverview) -> str:
-        return Welcome.objects.get(id=obj.get_parent().id).background_image.file.url
+        return (
+            settings.WAGTAILADMIN_BASE_URL
+            + Welcome.objects.get(id=obj.get_parent().id).background_image.file.url
+        )
+
+    def get_children(self, obj: CharacterOverview) -> list:
+        children = obj.get_children().live().specific()
+        serializer = ChooseCharacterModelSerializer(children, many=True)
+        return serializer.data if serializer.data else []
+
+    def get_selfUrl(self, obj: CharacterOverview) -> str:
+        return (
+            settings.WAGTAILADMIN_BASE_URL
+            + settings.API_BASE_URL
+            + "/character-overview/"
+            + str(obj.id)
+        )
 
 
 class ChooseCharacterModelSerializer(serializers.ModelSerializer):
     characterType = serializers.SerializerMethodField()
     characterImageUrl = serializers.SerializerMethodField()
     backgroundImageUrl = serializers.SerializerMethodField()
+    selfUrl = serializers.SerializerMethodField()
 
     class Meta:
         model = ChooseCharacter
@@ -62,6 +106,7 @@ class ChooseCharacterModelSerializer(serializers.ModelSerializer):
             "name",
             "characterImageUrl",
             "backgroundImageUrl",
+            "selfUrl",
         ]
         depth = 1
 
@@ -85,7 +130,16 @@ class ChooseCharacterModelSerializer(serializers.ModelSerializer):
             None,
         )
         return (
-            Welcome.objects.get(id=welcome_ancestor.id).background_image.file.url
+            settings.WAGTAILADMIN_BASE_URL
+            + Welcome.objects.get(id=welcome_ancestor.id).background_image.file.url
             if welcome_ancestor
             else ""
+        )
+
+    def get_selfUrl(self, obj: ChooseCharacter) -> str:
+        return (
+            settings.WAGTAILADMIN_BASE_URL
+            + settings.API_BASE_URL
+            + "/choose-character/"
+            + str(obj.id)
         )
