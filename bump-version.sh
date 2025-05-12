1fa9941a45b3fe6255632f1af0799338ba527c14
Stefan Dieterle <stefand@artcom.de>
2025-05-12 12:50:11 +0200

    Update bump version script

--- bump-version.sh
+++ bump-version.sh
@@ -3,6 +3,7 @@
 set -eo pipefail
 
 uv version --bump "$1"
-git add pyproject.toml .uv.lock
+uv lock
+git add pyproject.toml uv.lock
 git commit -m "Bump version to $(uv version)"
 git tag -a "v$(uv version)" -m "$(uv version)"
