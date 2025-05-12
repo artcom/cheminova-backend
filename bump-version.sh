5f9afa0264f0be5ef1d297ab743401377c8db7b6
Stefan Dieterle <stefand@artcom.de>
2025-05-12 13:09:34 +0200

    Simplify commit message

--- bump-version.sh
+++ bump-version.sh
@@ -5,5 +5,5 @@ set -eo pipefail
 uv version --bump "$1"
 uv lock
 git add pyproject.toml uv.lock
-git commit -m "Bump version to $(uv version --short)"
+git commit -m "$(uv version --short)"
 git tag -a "v$(uv version --short)" -m "$(uv version --short)"
