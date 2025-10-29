298146d9945fe837259070534effa279b1e95366
Stefan Dieterle <stefand@artcom.de>
2025-10-29 15:03:17 +0100

    Allow logging

--- src/cheminova/settings/base.py
+++ src/cheminova/settings/base.py
@@ -86,6 +86,20 @@ TEMPLATES = [
 WSGI_APPLICATION = "cheminova.wsgi.application"
 
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
+
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
