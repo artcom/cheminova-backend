8de342224bc8a189e265e9f9faa2d7abd39b190f
Stefan Dieterle <stefand@artcom.de>
2025-11-13 17:02:10 +0100

    Add admin user management command

---
+++ src/experience/management/commands/admin_user.py
@@ -0,0 +1,62 @@
+import logging
+
+from django.core.management.base import BaseCommand, CommandError
+
+from .users.admin import create_or_update_admin
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+
+class Command(BaseCommand):
+    help = "Create or update an admin user."
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "-p",
+            "--password",
+            type=str,
+            required=True,
+            help="Password for the admin user.",
+        )
+        parser.add_argument(
+            "-u",
+            "--username",
+            default="admin",
+            type=str,
+            help="Username of the admin user.",
+        )
+        parser.add_argument(
+            "-e",
+            "--email",
+            default="",
+            type=str,
+            help="Email address of the admin user.",
+        )
+        parser.add_argument(
+            "-f",
+            "--first-name",
+            default="",
+            type=str,
+            help="First name of the admin user.",
+        )
+        parser.add_argument(
+            "-l",
+            "--last-name",
+            default="",
+            type=str,
+            help="Last name of the admin user.",
+        )
+
+    def handle(self, *args, **options):
+        try:
+            create_or_update_admin(
+                username=options["username"],
+                email=options.get("email"),
+                first_name=options.get("first_name"),
+                last_name=options.get("last_name"),
+                password=options["password"],
+            )
+        except Exception as e:
+            raise CommandError(f"Error initializing admin user: {e}")
---
+++ src/experience/management/commands/users/admin.py
@@ -0,0 +1,27 @@
+from logging import getLogger
+
+from django.contrib.auth.models import User
+
+logger = getLogger(__name__)
+
+
+def create_or_update_admin(
+    username: str, email: str, first_name: str, last_name: str, password: str
+):
+    try:
+        admin = User.objects.get(username=username)
+        admin.email = email
+        admin.first_name = first_name
+        admin.last_name = last_name
+        admin.set_password(password)
+        admin.save()
+        logger.info("Updated admin user.")
+    except User.DoesNotExist:
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+        )
+        logger.info("Created admin user.")
